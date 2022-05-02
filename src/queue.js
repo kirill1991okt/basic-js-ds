89cbeee02c93695eee32825d6c63e9ebe4bd82b3
Kirill <kirill1991okt@gmail.com>
2022-05-03 02:51:59 +0300

    add: queuey

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,10 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const {
+  ListNode
+} = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +18,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    let current = this.head;
+
+    while (current) {
+      return current.next && current;
+
+    }
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value); // creates the node using class Node
+
+    if (this.head) { // if the first Node exitsts
+      this.tail.next = node; // inserts the created node after the tail of our Queue
+      this.tail = node; // now the created node is the last node
+    } else { // if there are no nodes in the Queue
+      this.head = node; // the created node is a head 
+      this.tail = node; // also the created node is a tail in Queue because it is single.
+    }
+
+    this.length++; // increases the length of Queue by 1
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head; // saves the link to the head which we need to remove
+    this.head = this.head.next; // moves the head link to the second Node in the Queue
+    this.length--; // decreaments the length of our Queue
+
+    return current.value; // returns the removed Node's value
   }
 }
 
